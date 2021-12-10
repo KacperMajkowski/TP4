e2ea0eaee636b73f21f31f17f1e2669f3225d648
KacperMajkowski <kacper20015@o2.pl>
2021-12-10 09:14:44 +0100

    Początki bywają ... różne

--- src/main/java/com/project/app/App.java
+++ src/main/java/com/project/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Początki bywają ... różne" );
+        System.out.println( "Początki bywają ... różne0" );
     }
 }
