85394c455c025881f15d6381fa0b327996022868
KacperMajkowski <kacper20015@o2.pl>
2021-12-10 09:15:15 +0100

    Git test

--- src/main/java/com/project/app/App.java
+++ src/main/java/com/project/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Początki bywają ... różne0" );
+        System.out.println( "Git test" );
     }
 }
